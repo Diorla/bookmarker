af2dd7005d10bf0e4e14d0e7c73ee4f4bcd35c0e
Ade Adeola <adedotster@gmail.com>
2022-09-20 11:52:37 +0100

    set default values for url

--- src/services/addUrl.ts
+++ src/services/addUrl.ts
@@ -12,7 +12,19 @@ const addUrl = async (userId: string, data: UrlProps, docId: string) => {
   const db = getFirestore();
   const docRef = doc(db, `users/${userId}/links`, docId);
 
-  return setDoc(docRef, { id: docId, ...data }, { merge: true });
+  const {
+    title = "",
+    url = "",
+    tags = [],
+    favicon = "",
+    description = "",
+  } = data;
+
+  return setDoc(
+    docRef,
+    { id: docId, title, url, tags, favicon, description },
+    { merge: true }
+  );
 };
 
 export default addUrl;
