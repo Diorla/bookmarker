79e2808e7e12455dfd8e474084b4ac4cd0e8e7df
Ade Adeola <adedotster@gmail.com>
2022-09-25 10:29:16 +0100

    styled components added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "styled-components";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const theme = {
+  primaryLight: "#4db6ac",
+  primary: "#00796b",
+  primaryDark: "#004d40",
+  secondaryLight: "#f06292",
+  secondary: "#e91e63",
+  secondaryDark: "#c2185b",
+  white: "#ffffff",
+  greyLight1: "#e4ebf5",
+  greyLight2: "#c8d0e7",
+  greyLight3: "#bec8e4",
+  greyDark: "#9baacf",
+  black: "#000000",
+  error: "#f44336",
+};
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
-)
+);
